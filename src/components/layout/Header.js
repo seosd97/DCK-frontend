2fa10371653bace7da8b291370021d75bd87a6af
seosd <seosd97@gmail.com>
2020-05-04 22:25:35 +0900

    header logo set to link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Header.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default () => {
   return (
     <header>
-      <img
-        id="logo-img"
-        src={process.env.PUBLIC_URL + '/dck_circle.png'}
-        width="50"
-        height="50"
-      ></img>
+      <Router>
+        <Link to="/">
+          <img
+            id="logo-img"
+            src={process.env.PUBLIC_URL + '/dck_circle.png'}
+            width="50"
+            height="50"
+          ></img>
+        </Link>
+      </Router>
     </header>
   );
 };
