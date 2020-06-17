a4d7da08b414abbc3567c836e9ab08ac4a83488f
seosd <seosd97@gmail.com>
2020-06-17 22:57:42 +0900

    팀 상세 페이지 작업 중

--- src/components/Team/TeamDetail.js
+++ src/components/Team/TeamDetail.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Axios from 'axios';
 import _ from 'underscore';
+import numeral from 'numeral';
+import './TeamDetail.css';
 
 class TeamDetail extends React.Component {
   constructor() {
@@ -19,7 +21,26 @@ class TeamDetail extends React.Component {
   }
 
   render() {
-    return <div>{!_.isEmpty(this.state.teamData) && this.state.teamData.name}</div>;
+    const { teamData } = this.state;
+    return (
+      <div className="team-detail-root flex-col">
+        {!_.isEmpty(teamData) && (
+          <React.Fragment>
+            <div className="team-detail-title">
+              <h1 className="team-title-name text-align-left">{teamData.name}</h1>
+              <h2 className="team-title-season text-align-left">{`${numeral(1).format(
+                '0o'
+              )} place at ${teamData.TournamentGroup.name}`}</h2>
+            </div>
+            <div>
+              {teamData.Summoners.map((s, i) => {
+                return <div key={i}>{s.name}</div>;
+              })}
+            </div>
+          </React.Fragment>
+        )}
+      </div>
+    );
   }
 }
 
