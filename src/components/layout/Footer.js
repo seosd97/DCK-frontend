a5c14447ec333a367fb43e2a7e080d06c8c3da01
seosd <seosd97@gmail.com>
2020-10-04 21:17:54 +0900

    add footer and link social links

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <footer className="flex-col flex-j-c flex-align-c width-100">
+      <p>
+        Contact us by <a href="mailto:seosd97@gmail.com">e-mail</a>
+      </p>
+      <p>Released under the MIT License</p>
+      <p>Copyright (c) 2020 SeungDuk Seo</p>
+      <div id="social-links" className="flex-row flex-align-c">
+        <a href="https://www.youtube.com/user/cjwsc0410" target="_blank" rel="noopener noreferrer">
+          <img
+            src={`${process.env.PUBLIC_URL}/icons/youtube_icon_white.png`}
+            alt="social_link_youtube"
+          ></img>
+        </a>
+        <a href="https://discord.gg/JgKqqp" target="_blank" rel="noopener noreferrer">
+          <img
+            src={`${process.env.PUBLIC_URL}/icons/discord_icon_white.png`}
+            alt="social_link_dicord"
+          ></img>
+        </a>
+        <a href="https://github.com/seosd97/DCK-frontend" target="_blank" rel="noopener noreferrer">
+          <img
+            src={`${process.env.PUBLIC_URL}/icons/github_icon_white.png`}
+            alt="social_link_github"
+          ></img>
+        </a>
+      </div>
+    </footer>
+  );
+};
--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 import Header from './Header';
 
 function MainLayout({ children }) {
@@ -6,6 +7,7 @@ function MainLayout({ children }) {
     <React.Fragment>
       <Header />
       <main>{children}</main>
+      <Footer />
     </React.Fragment>
   );
 }
