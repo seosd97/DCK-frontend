503342390c519e3f6bf173a480cb0d83ec375e60
seosd <seosd97@gmail.com>
2020-10-06 22:01:18 +0900

    remove round button

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import RoundButton from './ui/RoundButton';
 import './Main.css';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <section className="main-root">
-        <div className="black-layer">
-          <div className="main-info-container flex-col width-100">
-            <img
-              className="main-logo"
-              src={`${process.env.PUBLIC_URL}/dck_logo_300.jpg`}
-              alt="logo_300"
-              width="100"
-              height="100"
-            ></img>
-            <div className="main-title">
-              <h1>DCK</h1>
-              <h2>The our own League of Legends tournament</h2>
-            </div>
-            <div className="flex-row flex-a-c flex-wrap width-100">
-              <RoundButton to="/matches" desc="경기 결과" />
-              <RoundButton to="/rankings" desc="소환사 기록" />
-            </div>
+export default () => {
+  return (
+    <section className="main-root">
+      <div className="black-layer">
+        <div className="main-info-container flex-col width-100">
+          <img
+            className="main-logo"
+            src={`${process.env.PUBLIC_URL}/dck_logo_300.jpg`}
+            alt="logo_300"
+            width="100"
+            height="100"
+          ></img>
+          <div className="main-title">
+            <h1>DCK</h1>
+            <h2>The our own League of Legends tournament</h2>
+          </div>
+          <div className="flex-row flex-a-c flex-wrap width-100">
+            <Link to="/matches" className="content-button">
+              경기 결과
+            </Link>
+            <Link to="/rankings" className="content-button">
+              소환사 기록
+            </Link>
           </div>
         </div>
-      </section>
-    );
-  }
-}
-
-export default Main;
+      </div>
+    </section>
+  );
+};
